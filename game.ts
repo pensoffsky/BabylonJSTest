9e0a678693783c1269abb4a3e2066b2cb6ed9865
Elmar Kuesters <elmar.kuesters@sap.com>
2019-05-18 12:56:03 +0200

    stuff

--- game.ts
+++ game.ts
@@ -18,10 +18,17 @@ class Game {
 
         // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
         this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this._scene);
+    
+         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
+         this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
     }
 
     createScene(): void {
-        // this._scene.debugLayer.show();
+        this._scene.debugLayer.show();
+        //this._scene.ambientColor = new BABYLON.Color3(1,1,1);
+
+        this._scene.gravity = new BABYLON.Vector3(0, -9, 0);
+        this._scene.collisionsEnabled = true;
 
         // Target the camera to scene origin.
         this._camera.setTarget(BABYLON.Vector3.Zero());
@@ -29,8 +36,9 @@ class Game {
         // Attach the camera to the canvas.
         this._camera.attachControl(this._canvas, false);
 
-        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
+       
+
+        var spotLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(10,1,1), this._scene);
 
         // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
         let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
@@ -39,13 +47,60 @@ class Game {
         // Move the sphere upward 1/2 of its height.
         sphere.position.y = 1;
 
+        var myMaterial = new BABYLON.StandardMaterial("myMaterial", this._scene);
+        myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
+        // myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
+        // myMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1);
+        // myMaterial.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.53);
+
         let box = BABYLON.MeshBuilder.CreateBox("box", {}, this._scene); 
         box.position.y = 2;
-        box.position.x = 2;
+        box.position.x = -5;
+        box.material = myMaterial;
         
+
+        var animationBox = new BABYLON.Animation("myAnimation", "position.x", 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+        // An array with all animation keys
+        var keys = []; 
+        keys.push({
+            frame: 0,
+            value: -5
+        });
+        keys.push({
+            frame: 60,
+            value: 0
+        });
+        keys.push({
+            frame: 1000,
+            value: 5
+        });
+        animationBox.setKeys(keys);
+        box.animations = [animationBox];
+        var anim = this._scene.beginAnimation(box, 0, 1000, false);
+
         // Create a built-in "ground" shape.
-        let ground = BABYLON.MeshBuilder.CreateGround('ground1',
-            { width: 6, height: 6, subdivisions: 2 }, this._scene);
+        // let ground = BABYLON.MeshBuilder.CreateGround('ground1',
+        //     { width: 6, height: 6, subdivisions: 2 }, this._scene);
+
+        // Default Environment - adds a skybox and a ground
+        var environment = this._scene.createDefaultEnvironment({ 
+            enableGroundShadow: true,
+            groundYBias: 1 
+        });
+        if(environment === null) {
+            return;
+        }
+        environment.setMainColor(BABYLON.Color3.FromHexString("#74b9ff"));
+
+        var cornellBox = BABYLON.SceneLoader.ImportMesh(
+            "",
+            "https://models.babylonjs.com/CornellBox/",
+            "cornellBox.glb",
+            this._scene);
+
+        // Enable VR
+        var vrHelper = this._scene.createDefaultVRExperience({createDeviceOrientationCamera:false});
+        vrHelper.enableTeleportation({floorMeshes: [environment.ground!]});
     }
 
 
@@ -58,6 +113,14 @@ class Game {
         // The canvas/window resize event handler.
         window.addEventListener('resize', () => {
             this._engine.resize();
+        });
+
+        // The canvas/window resize event handler.
+        window.addEventListener('click', () => {
+            var pickResult = this._scene.pick(this._scene.pointerX, this._scene.pointerY);
+            if( pickResult && pickResult.hit ) {
+                console.log(pickResult.pickedMesh!.name);
+            }
         });
     }
 }
